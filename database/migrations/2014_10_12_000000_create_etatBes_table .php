dbc4c1896fa66e36d08a376d4c6ea534e3fdb926
davidtino87 <65030518+davidtino87@users.noreply.github.com>
2023-01-02 22:24:01 +0200

    les tables produit et etat de besoin

---
+++ database/migrations/2014_10_12_000000_create_etatBes_table .php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('etatBes', function (Blueprint $table) {
+            $table->id();
+            $table->string('numero')->unique();
+            $table->string('agent')->constrained()->onDelete('cascade');
+            $table->string('ligne')->nullable();
+            $table->text('comment')->nullable();
+            $table->boolean('niv1')->default(false);
+            $table->boolean('niv2')->default(false);
+            $table->boolean('niv3')->default(false);
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('etatBes');
+    }
+};
---
+++ database/migrations/2014_10_12_000000_create_products_table .php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('designation');
+            $table->string('model');
+            $table->string('description')->nullable();
+            $table->integer('quantite')->nullable();;
+            $table->string('unite');
+            $table->float('prix')->nullable();;
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
