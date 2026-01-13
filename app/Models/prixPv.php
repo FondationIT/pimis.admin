5aaebdbe08aa0cb982b39290dedd2a6af85dc7f4
Given Masheka <official.given.m@gmail.com>
2026-01-13 09:08:21 +0200

    error correct 2

--- app/Models/prixPv.php
+++ app/Models/prixPv.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class prixPv extends Model
+class PrixPv extends Model
 {
     use HasFactory;
 
